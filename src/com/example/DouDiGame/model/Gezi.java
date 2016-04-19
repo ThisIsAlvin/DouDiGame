87b651eeb4b842c459a69c9456fc490080a2b321
ThisIsAlvin <2379243059@qq.com>
2016-04-19 13:39:34 +0800

    在service的play中添加了查询格子的方法

--- src/com/example/DouDiGame/model/Gezi.java
+++ src/com/example/DouDiGame/model/Gezi.java
@@ -4,6 +4,8 @@ package com.example.DouDiGame.model;
  * Created by viv on 16-4-19.
  */
 public class Gezi {
+    /*格子id*/
+    public  String geziId;
     /*标记所属：0表示没有所属，1表示属于玩家1,2表示属于玩家2*/
     public int userId;
     /*标记价格*/
--- src/com/example/DouDiGame/service/Play.java
+++ src/com/example/DouDiGame/service/Play.java
@@ -1,16 +1,36 @@
 package com.example.DouDiGame.service;
 
 import com.example.DouDiGame.model.Game;
+import com.example.DouDiGame.model.Gezi;
+
+import java.util.List;
 
 /**
  * Created by viv on 16-4-19.
  */
 public class Play {
     public Game game;
+    public List<Gezi> geziList;
 
     public Play(Game game){
         this.game = game;
+        geziList = game.geziList;
     }
 
+    /*传入格子ID，查找对应的格子信息*/
+    public Gezi get_gezi(String geziId){
+
+        for (Gezi gezi :
+                geziList) {
+            if(gezi.geziId==geziId){
+                return gezi;
+            }
+        }
+        return null;
+    }
+
+    public void go(){
+
+    }
 
 }
